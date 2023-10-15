0cd56aa50e0810cef0dcc5ea9c810a139c732ad7
AkhraAvidzba <akhraa1@yandex.ru>
2023-10-15 22:13:30 +0300

    test: add TestService unit test and QuestionDao Integration test

--- hw02/src/test/java/ru/otus/avidzba/service/QuestionServiceImplTest.java
+++ hw02/src/test/java/ru/otus/avidzba/service/QuestionServiceImplTest.java
@@ -1,18 +1,52 @@
 package ru.otus.avidzba.service;
 
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.otus.avidzba.dao.CsvQuestionDao;
 import ru.otus.avidzba.dao.QuestionDao;
+import ru.otus.avidzba.domain.Answer;
+import ru.otus.avidzba.domain.Question;
+import ru.otus.avidzba.domain.Student;
+import ru.otus.avidzba.domain.TestResult;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class QuestionServiceImplTest {
+    private final IOService ioService = Mockito.mock(StreamsIOService.class);
     private final QuestionDao questionDao = Mockito.mock(CsvQuestionDao.class);
-//    private final QuestionServiceImpl questionService = new QuestionServiceImpl(questionDao);
-//
-//    @Test
-//    void findAll_whenInvoke_returnAllQuestions() {
-//        when(questionDao.findAll()).thenReturn(List.of(new Question(1, "question1", List.of()), new Question(1, "question2", List.of())));
-//        assertThat(questionService.findAll().size(), equalTo(2));
-//        assertThat(questionService.findAll().get(0).getQuestion(), equalTo("question1"));
-//        assertThat(questionService.findAll().get(1).getQuestion(), equalTo("question2"));
-//    }
+
+    private final TestServiceImpl questionService = new TestServiceImpl(ioService, questionDao);
+
+    @Test
+    void executeTestFor_whenInvokeWithStudent_returnResultOfTest() {
+        List<Question> questions = makeQuestions();
+        Student student = makeStudent();
+
+        when(questionDao.findAll()).thenReturn(List.of(questions.get(0), questions.get(1)));
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString())).thenReturn(1);
+
+        TestResult testResult = questionService.executeTestFor(student);
+        assertThat(testResult.getRightAnswersCount(), equalTo(1));
+        assertThat(testResult.getAnsweredQuestions(), equalTo(questions));
+    }
+
+    private Student makeStudent() {
+        return new Student("first name", "last name");
+    }
+
+    private List<Question> makeQuestions() {
+        Answer answer1 = new Answer("answer1", false);
+        Answer answer2 = new Answer("answer1", true);
+        Answer answer3 = new Answer("answer3", true);
+        Answer answer4 = new Answer("answer4", false);
+        Question question1 = new Question("question1", List.of(answer1, answer2));
+        Question question2 = new Question("question2", List.of(answer3, answer4));
+        return List.of(question1, question2);
+    }
 }
