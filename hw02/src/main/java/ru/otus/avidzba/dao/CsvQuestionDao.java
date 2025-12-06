d44abf05e52418085c83a167158b10f798009f8a
ahra <akhraa1@yandex.ru>
2025-12-07 00:19:20 +0300

    add ClassPathResource instead of FileReader

--- hw02/src/main/java/ru/otus/avidzba/dao/CsvQuestionDao.java
+++ hw02/src/main/java/ru/otus/avidzba/dao/CsvQuestionDao.java
@@ -2,6 +2,7 @@ package ru.otus.avidzba.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
 import ru.otus.avidzba.config.TestFileNameProvider;
 import ru.otus.avidzba.dao.dto.QuestionDto;
@@ -10,6 +11,8 @@ import ru.otus.avidzba.exceptions.QuestionReadException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,15 +23,23 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        List<QuestionDto> beans;
-        try (FileReader reader = new FileReader(fileNameProvider.getTestFileName())) {
-            beans = new CsvToBeanBuilder(reader)
+        try (var reader = new InputStreamReader(
+                new ClassPathResource(fileNameProvider.getTestFileName())
+                        .getInputStream(), StandardCharsets.UTF_8)) {
+
+            var beans = new CsvToBeanBuilder<QuestionDto>(reader)
                     .withSeparator(';')
                     .withSkipLines(1)
-                    .withType(QuestionDto.class).build().parse();
-        } catch (IOException e) {
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+
+            return beans.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+
+        } catch (Exception e) {
             throw new QuestionReadException("Error reading csv file", e);
         }
-        return beans.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
     }
 }
