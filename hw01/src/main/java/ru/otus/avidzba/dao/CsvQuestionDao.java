c3a90ce28bf1cd59453c36b3ecea402fe750bfe7
ahra <akhraa1@yandex.ru>
2025-12-20 16:10:50 +0300

    fix hw01

--- hw01/src/main/java/ru/otus/avidzba/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/avidzba/dao/CsvQuestionDao.java
@@ -2,31 +2,41 @@ package ru.otus.avidzba.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
 import ru.otus.avidzba.config.TestFileNameProvider;
 import ru.otus.avidzba.dao.dto.QuestionDto;
 import ru.otus.avidzba.domain.Question;
 import ru.otus.avidzba.exceptions.QuestionReadException;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Repository
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        List<QuestionDto> beans;
-        try (FileReader reader = new FileReader(fileNameProvider.getTestFileName())) {
-            beans = new CsvToBeanBuilder(reader)
+        try (var reader = new InputStreamReader(
+                new ClassPathResource(fileNameProvider.getTestFileName())
+                        .getInputStream(), StandardCharsets.UTF_8)) {
+
+            var beans = new CsvToBeanBuilder<QuestionDto>(reader)
                     .withSeparator(';')
                     .withSkipLines(1)
-                    .withType(QuestionDto.class).build().parse();
-        } catch (IOException e) {
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+
+            return beans.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+
+        } catch (Exception e) {
             throw new QuestionReadException("Error reading csv file", e);
         }
-        return beans.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
     }
 }
